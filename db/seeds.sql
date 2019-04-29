7873100ed52d99d26e76da7f5f7bfe17a2cb3dc0
Barth37 <brian.arth@gmail.com>
2019-04-29 18:21:17 -0400

    initial setup

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Atomic Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fat Doug Burger', true);
+
